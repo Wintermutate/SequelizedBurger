926d79ac5ff9898fc6d90b132f5170ff79575680
Vikas Sundrani <sundrani.vikas@gmail.com>
2016-11-17 15:33:13 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Hamburger', false);
